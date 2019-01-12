2e911d0499fe9cbeea732024d2217eeb60f6f39d
Abbie Ma <ama@flex125.com>
2019-01-11 18:45:29 -0600

    creating the home page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png"/>
+            Build-a-Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage msg="Welcome to Your Vue.js App"/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    HomePage,
   },
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: #fff;
+  width: 1024px;
+  min-height: 300px;
 }
 </style>
